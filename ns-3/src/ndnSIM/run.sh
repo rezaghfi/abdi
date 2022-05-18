9d013e3f31a550647b71c4227a0f0e26594e9214
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-05-18 11:06:04 +0430

    1

---
+++ ns-3/src/ndnSIM/run.sh
@@ -0,0 +1,30 @@
+#bash
+
+cd ../..
+#NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=1-variousSpeed
+#NS_LOG=ndn-cxx.nfd.RFAStrategy ./waf --run=1-variousSpeed
+#NS_LOG=ndn-cxx.nfd.SAFStrategy ./waf --run=1-variousSpeed
+#NS_LOG=ndn-cxx.nfd.SMDPStrategy ./waf --run=1-variousSpeed
+#NS_LOG=ndn-cxx.nfd.LAMDPStrategy ./waf --run=1-variousSpeed
+#NS_LOG=ndn-cxx.nfd.AMIFStrategy ./waf --run=1-variousSpeed
+
+#NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=2-variousCache
+#NS_LOG=ndn-cxx.nfd.RFAStrategy ./waf --run=2-variousCache
+#NS_LOG=ndn-cxx.nfd.SAFStrategy ./waf --run=2-variousCache
+#NS_LOG=ndn-cxx.nfd.SMDPStrategy ./waf --run=2-variousCache
+#NS_LOG=ndn-cxx.nfd.LAMDPStrategy ./waf --run=2-variousCache
+#NS_LOG=ndn-cxx.nfd.AMIFStrategy ./waf --run=2-variousCache
+
+#NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=3-variousTopology
+#NS_LOG=ndn-cxx.nfd.RFAStrategy ./waf --run=3-variousTopology
+#NS_LOG=ndn-cxx.nfd.SAFStrategy ./waf --run=3-variousTopology
+#NS_LOG=ndn-cxx.nfd.SMDPStrategy ./waf --run=3-variousTopology
+#NS_LOG=ndn-cxx.nfd.LAMDPStrategy ./waf --run=3-variousTopology
+#NS_LOG=ndn-cxx.nfd.AMIFStrategy ./waf --run=3-variousTopology
+
+NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=4-variousLinkFailure
+#NS_LOG=ndn-cxx.nfd.RFAStrategy ./waf --run=4-variousLinkFailure
+#NS_LOG=ndn-cxx.nfd.SAFStrategy ./waf --run=4-variousLinkFailure
+#NS_LOG=ndn-cxx.nfd.SMDPStrategy ./waf --run=4-variousLinkFailure
+#NS_LOG=ndn-cxx.nfd.LAMDPStrategy ./waf --run=4-variousLinkFailure
+#NS_LOG=ndn-cxx.nfd.AMIFStrategy ./waf --run=4-variousLinkFailure
