3e072e1d965fa9e1c3b46067644414f9d520fe2d
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-05-18 11:12:56 +0430

    1

--- ns-3/src/ndnSIM/run.sh
+++ ns-3/src/ndnSIM/run.sh
@@ -1,6 +1,7 @@
 #bash
-
-cd ../..
+cd NFD
+git checkout master1
+cd ../../../
 #NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=1-variousSpeed
 #NS_LOG=ndn-cxx.nfd.RFAStrategy ./waf --run=1-variousSpeed
 #NS_LOG=ndn-cxx.nfd.SAFStrategy ./waf --run=1-variousSpeed
@@ -28,3 +29,6 @@ NS_LOG=ndn-cxx.nfd.BestRouteStrategy ./waf --run=4-variousLinkFailure
 #NS_LOG=ndn-cxx.nfd.SMDPStrategy ./waf --run=4-variousLinkFailure
 #NS_LOG=ndn-cxx.nfd.LAMDPStrategy ./waf --run=4-variousLinkFailure
 #NS_LOG=ndn-cxx.nfd.AMIFStrategy ./waf --run=4-variousLinkFailure
+cd src/ndnSIM/NFD
+git checkout master
+cd ..
